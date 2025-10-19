fa42aced8f3c8fd8562623a17046084f66bd35c3
SrijanSriv <srijansrivastav01@gmail.com>
2025-10-19 23:40:32 +0530

    add: pdfplumber

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -3,16 +3,20 @@ from sqlalchemy.orm import Session
 from app.db.base import get_db
 from app.models.document import Document
 import mimetypes
+import pdfplumber
 
 router = APIRouter(prefix="/api/v1/upload")
 
 @router.post("")
 async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    file_content = await file.read()
     file_name = file.filename
-    file_mime_type = mimetypes.guess_type(file_name)[0] or "application/octet-stream"
+    file_mime_type = mimetypes.guess_type(file.filename)[0]
+    extracted_file_content = ""
 
-    document = Document(filename=file_name, mime_type=file_mime_type, raw_text=file_content, embedding=None)
+    for page in pdfplumber.open(file.file).pages:
+        extracted_file_content += page.extract_text()
+
+    document = Document(filename=file_name, mime_type=file_mime_type, raw_text=extracted_file_content, embedding=None)
     db.add(document)
     db.commit()
 
